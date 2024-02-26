0ec5beeb02a327c08a9b5cacb27a7d10c7aa20f3
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 20:56:01 -0300

    update

--- src/ResorcesManager/GUI.lua
+++ src/ResorcesManager/GUI.lua
@@ -1,9 +1,23 @@
-local resorcesManagerGUIModule = {}
+-- local resorcesManagerGUIModule = {}
 
-local resorcesManagerGUIModule.createResorcesManagerFrame = function(main)
+-- local resorcesManagerGUIModule.createResorcesManagerFrame = function(main)
+--     frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()
+--     frame:addLabel():setText("Resorce Menager"):setPosition(2, 2)
+--     return main, frame
+-- end
+
+-- return resorcesManagerGUIModule
+
+local generatorGUIModule = {}
+
+local generatorModuleObject = require("/CC/src/GeneratorManager/Generator")
+
+function generatorGUIModule.createGeneratorFrame(main)
     frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()
-    frame:addLabel():setText("Resorce Menager"):setPosition(2, 2)
+
+    frame:addLabel():setText("Generator Manager"):setPosition(2, 2)
+
     return main, frame
 end
 
-return resorcesManagerGUIModule
+return generatorGUIModule
