30af385fedcd21a921040478dde4e6f536acffc2
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 13:29:43 -0300

    update

--- src/main.lua
+++ src/main.lua
@@ -1,41 +1,28 @@
---------------------------------------------------------------------------------
--- GERADOR MAIN 
---------------------------------------------------------------------------------
-
--- shell.run("/CC/src/GeneratorManager/Generator.lua")
-
---------------------------------------------------------------------------------
--- GERADOR GUI 
---------------------------------------------------------------------------------
-
 -- shell.run("/CC/src/GeneratorManager/GUI.lua")
 
---------------------------------------------------------------------------------
--- GUI TESTE
---------------------------------------------------------------------------------
-
--- shell.run("/CC/src/tests/guiTest/menager.lua")
-
--- local basalt = require("/CC/Modules/basaltMaster")
-
--- local mainFrame = basalt.createFrame()
--- local aButton = mainFrame:addButton():setSize(10, 3)
--- local w, h = aButton:getSize()
--- -- basalt.debug("Button position: w=" .. w .. ", h=" .. h) -- prints "Button position: w=10, h=3"
--- print(w)
--- print(h)
-
---------------------------------------------------------------------------------
--- MAIN GUI
---------------------------------------------------------------------------------
+----------------------------------------
+--MODULES
+----------------------------------------
 
 local basalt = require("/CC/Modules/basaltMaster") -- we need basalt here
 local debugMenu = require('/CC/Modules/basaltDebug'):setBasalt(basalt)
 
+----------------------------------------
+--MAIN FRAME
+----------------------------------------
+
 local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
 local w, h = main:getSize()
 
+----------------------------------------
+--DEBUG FRAME
+----------------------------------------
+
 local debugFrame = debugMenu:createDebugMenu(main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
+
+----------------------------------------
+--SIDE BAR
+----------------------------------------
 
 local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
 :onGetFocus(function(self)
@@ -45,25 +32,21 @@ end)
     self:setPosition(w, 1)
 end)
 
--- Once again we add 3 frames, the first one should be immediatly visible
+
 local sub = {
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"),
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),
-    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),
+    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"),        -- MAIN
+    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), -- GENRATOR MENAGER
+    main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), -- RESORCE MENAGER
 }
 
---This part of the code adds buttons based on the sub table.
 local y = 2
 for k,v in pairs(sub)do
-    sidebar:addButton():setText("Example "..k) -- creating the button and adding a name k is just the index
+    sidebar:addButton():setText("Example "..k)
     :setBackground(colors.black)
     :setForeground(colors.lightGray)
     :setSize("{parent.w - 3}", 3)
     :setPosition(2, y)
-    :onClick(function() -- here we create a on click event which hides ALL sub frames and then shows the one which is linked to the button
+    :onClick(function()
         for a, b in pairs(sub)do
             b:hide()
             v:show()
@@ -71,6 +54,10 @@ for k,v in pairs(sub)do
     end)
     y = y + 4
 end
+
+----------------------------------------
+--SIDE BAR
+----------------------------------------
 
 sub[1]:addButton():setPosition(2, 2)
 
@@ -80,70 +67,3 @@ sub[3]:addLabel():setText("Now we're on example 3!"):setPosition(2, 2)
 sub[3]:addButton():setText("No functionality"):setPosition(2, 4):setSize(18, 3)
 
 basalt.autoUpdate()
-
---------------------------------------------------------------------------------
--- SCROLL LIST 
---------------------------------------------------------------------------------
-
--- local basalt = require("/CC/Modules/basaltMaster") -- we need basalt here
--- local mainFrame = basalt.createFrame()
--- local scrollableFrame = mainFrame:addScrollableFrame():setBackground(colors.blue)
-
--- local aList = scrollableFrame:addList()
--- aList:addItem("Item 1")
--- aList:addItem("Item 2", colors.yellow)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
--- aList:addItem("Item 3", colors.yellow, colors.green)
-
--- scrollableFrame:setDirection("vertical")
--- basalt.autoUpdate()
-
---------------------------------------------------------------------------------
--- MAIN GUI
---------------------------------------------------------------------------------
-
--- local basalt = require("/CC/Modules/basaltMaster") -- we need basalt here
-
--- local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black}) -- we change the default bg and fg color for frames
-
--- local sub = { -- here we create a table where we gonna add some frames
---     main:addFrame():setPosition(1, 2):setSize("{parent.w}", "{parent.h - 1}"), -- obviously the first one should be shown on program start
---     main:addFrame():setPosition(1, 2):setSize("{parent.w}", "{parent.h - 1}"):hide(),
---     main:addFrame():setPosition(1, 2):setSize("{parent.w}", "{parent.h - 1}"):hide(),
--- }
-
--- local function openSubFrame(id) -- we create a function which switches the frame for us
---     if(sub[id]~=nil)then
---         for k,v in pairs(sub)do
---             v:hide()
---         end
---         sub[id]:show()
---     end
--- end
-
--- local menubar = main:addMenubar():setScrollable() -- we create a menubar in our main frame.
---     :setSize("{parent.w}", 1)
---     :onChange(function(self, val)
---         openSubFrame(self:getItemIndex()) -- here we open the sub frame based on the table index
---     end)
---     :addItem("Example 1")
---     :addItem("Example 2")
---     :addItem("Example 3")
-
--- -- Now we can change our sub frames, if you want to access a sub frame just use sub[subid], some examples:
--- sub[1]:addButton():setPosition(2, 2)
-
--- sub[2]:addLabel():setText("Hello World!"):setPosition(2, 2)
-
--- sub[3]:addLabel():setText("Now we're on example 3!"):setPosition(2, 2)
--- sub[3]:addButton():setText("No functionality"):setPosition(2, 4):setSize(18, 3)
-
--- basalt.autoUpdate()
