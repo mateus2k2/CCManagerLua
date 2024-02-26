fd5c90f506e8e44a17c9b7302653f0b9581b769f
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 20:16:20 -0300

    update

--- src/GeneratorManager/Generator.lua
+++ src/GeneratorManager/Generator.lua
@@ -1,2 +1,30 @@
+local gerenatorModule = {}
+
 local inductionMatrix = peripheral.wrap("inductionPort_0")
-print(inductionMatrix.getEnergy())
+if inductionMatrix == nil then error("Batery not found") end
+
+transmiter = peripheral.find("redstoneIntegrator")
+if transmiter == nil then error("Transmiter not found") end
+
+function gerenatorModule.getBatteryFillLevel()
+    return inductionMatrix.getEnergy()
+end
+
+function gerenatorModule.getBatteryMaxFillLevel()
+    return inductionMatrix.getMaxEnergy()
+end
+
+function gerenatorModule.turnOnGenerator()
+    integrator.setOutput("south", true)
+end
+
+function gerenatorModule.turnOffGenerator()
+    integrator.setOutput("south", false)
+end
+
+function gerenatorModule.getStateGenerator()
+    valor = integrator.getAnalogInput("south")
+    return (valor > 0)
+end
+
+return gerenatorModule
--- src/main.lua
+++ src/main.lua
@@ -1,62 +1,66 @@
 -- shell.run("/CC/src/GeneratorManager/GUI.lua")
 
-----------------------------------------
---MODULES
-----------------------------------------
+local generatorModuleObject = require("/CC/src/GeneratorManager/Generator")
 
-local basalt = require("/CC/Modules/basaltMaster") -- we need basalt here
-local debugMenu = require('/CC/Modules/basaltDebug'):setBasalt(basalt)
+print(generatorModuleObject.getBatteryFillLevel())
 
-----------------------------------------
---MAIN FRAME
-----------------------------------------
+-- ----------------------------------------
+-- --MODULES
+-- ----------------------------------------
 
-local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
-local w, h = main:getSize()
+-- local basalt = require("/CC/Modules/basaltMaster") -- we need basalt here
+-- local debugMenu = require('/CC/Modules/basaltDebug'):setBasalt(basalt)
 
-----------------------------------------
---DEBUG FRAME
-----------------------------------------
+-- ----------------------------------------
+-- --MAIN FRAME
+-- ----------------------------------------
 
-local debugFrame = debugMenu:createDebugMenu(main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
+-- local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
+-- local w, h = main:getSize()
 
-----------------------------------------
---SIDE BAR
-----------------------------------------
+-- ----------------------------------------
+-- --DEBUG FRAME
+-- ----------------------------------------
 
-local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
-:onGetFocus(function(self)
-    self:setPosition("{parent.w - (self.w-1) - 1}", 1)
-end)
-:onLoseFocus(function(self)
-    self:setPosition(w, 1)
-end)
+-- local debugFrame = debugMenu:createDebugMenu(main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
+
+-- ----------------------------------------
+-- --SIDE BAR
+-- ----------------------------------------
+
+-- local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
+-- :onGetFocus(function(self)
+--     self:setPosition("{parent.w - (self.w-1) - 1}", 1)
+-- end)
+-- :onLoseFocus(function(self)
+--     self:setPosition(w, 1)
+-- end)
 
 
-local sub = {
-    {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")       , title = "Main"},      -- MAIN
-    {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), title = "Generator"}, -- GENRATOR MENAGER
-    {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), title = "Resorses"},  -- RESORCE MENAGER
-}
+-- local sub = {
+--     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")       , title = "Main"},      -- MAIN
+--     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), title = "Generator"}, -- GENRATOR MENAGER
+--     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(), title = "Resorses"},  -- RESORCE MENAGER
+-- }
 
-local y = 2
-for k,v in pairs(sub)do
-    sidebar:addButton():setText(v.title)
-    :setBackground(colors.black)
-    :setForeground(colors.lightGray)
-    :setSize("{parent.w - 3}", 3)
-    :setPosition(2, y)
-    :onClick(function()
-        for a, b in pairs(sub)do
-            b.frame:hide()
-            v.frame:show()
-        end
-    end)
-    y = y + 4
-end
+-- local y = 2
+-- for k,v in pairs(sub)do
+--     sidebar:addButton():setText(v.title)
+--     :setBackground(colors.black)
+--     :setForeground(colors.lightGray)
+--     :setSize("{parent.w - 3}", 3)
+--     :setPosition(2, y)
+--     :onClick(function()
+--         for a, b in pairs(sub)do
+--             b.frame:hide()
+--             v.frame:show()
+--         end
+--     end)
+--     y = y + 4
+-- end
 
-----------------------------------------
---MAIN FRAME
-----------------------------------------
+-- ----------------------------------------
+-- --MAIN FRAME
+-- ----------------------------------------
 
-basalt.autoUpdate()
+-- basalt.autoUpdate()
