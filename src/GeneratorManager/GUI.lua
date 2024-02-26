da6453dc3087725f29532763d146c13595c2303f
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 20:40:06 -0300

    update

--- src/GeneratorManager/GUI.lua
+++ src/GeneratorManager/GUI.lua
@@ -2,14 +2,17 @@ local generatorGUIModule = {}
 
 local generatorModuleObject = require("/CC/src/GeneratorManager/Generator")
 
-function generatorGUIModule.createGeneratorFrame(frame)
-    frame:setText("Now we're on example 3!"):setPosition(2, 2)
-    return frame
+function generatorGUIModule.createGeneratorFrame(main)
+    frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()
+
+    frame:addLabel():setText("Now we're on example 3!"):setPosition(2, 2)
+
+    return main, frame
 end
 
-function generatorGUIModule.teste(x)
-    x = x + 1
-    return x
-end
+-- function generatorGUIModule.teste(x)
+--     x = x + 1
+--     return x
+-- end
 
 return generatorGUIModule
--- src/main.lua
+++ src/main.lua
@@ -7,69 +7,70 @@ local debugMenu = require('/CC/Modules/basaltDebug'):setBasalt(basalt)
 
 local generatorGUIModule = require("/CC/src/GeneratorManager/GUI")
 
-local x = 2
-print(x)
-x = generatorGUIModule.teste(x)
-print(x)
+-- local x = 2
+-- print(x)
+-- x = generatorGUIModule.teste(x)
+-- print(x)
 
--- ----------------------------------------
--- --MAIN FRAME
--- ----------------------------------------
+----------------------------------------
+--MAIN FRAME
+----------------------------------------
 
--- local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
--- local w, h = main:getSize()
+local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
+local w, h = main:getSize()
 
--- ----------------------------------------
--- --DEBUG FRAME
--- ----------------------------------------
+----------------------------------------
+--DEBUG FRAME
+----------------------------------------
 
--- local debugFrame = debugMenu:createDebugMenu(main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
+local debugFrame = debugMenu:createDebugMenu(main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
 
--- ----------------------------------------
--- --SIDE BAR
--- ----------------------------------------
+----------------------------------------
+--SIDE BAR
+----------------------------------------
 
--- local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
--- :onGetFocus(function(self)
---     self:setPosition("{parent.w - (self.w-1) - 1}", 1)
--- end)
--- :onLoseFocus(function(self)
---     self:setPosition(w, 1)
--- end)
+local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
+:onGetFocus(function(self)
+    self:setPosition("{parent.w - (self.w-1) - 1}", 1)
+end)
+:onLoseFocus(function(self)
+    self:setPosition(w, 1)
+end)
 
+main, frameGenerator = generatorGUIModule.createGeneratorFrame(main)
 
--- local sub = {
---     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")                                           , title = "Main"},      -- MAIN
---     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide() , title = "Generator"}             , -- GENERATOR MENAGER
---     {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()                                    , title = "Resorses"},  -- RESORCE MENAGER
--- }
+local sub = {
+    {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"),         title = "Main"},      -- MAIN
+    {frame = frameGenerator,                                                                title = "Generator"}, -- GENERATOR MENAGER
+    {frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide(),  title = "Resorses"},  -- RESORCE MENAGER
+}
 
--- local y = 2
--- for k,v in pairs(sub)do
---     sidebar:addButton():setText(v.title)
---     :setBackground(colors.black)
---     :setForeground(colors.lightGray)
---     :setSize("{parent.w - 3}", 3)
---     :setPosition(2, y)
---     :onClick(function()
---         for a, b in pairs(sub)do
---             b.frame:hide()
---             v.frame:show()
---         end
---     end)
---     y = y + 4
--- end
+local y = 2
+for k,v in pairs(sub)do
+    sidebar:addButton():setText(v.title)
+    :setBackground(colors.black)
+    :setForeground(colors.lightGray)
+    :setSize("{parent.w - 3}", 3)
+    :setPosition(2, y)
+    :onClick(function()
+        for a, b in pairs(sub)do
+            b.frame:hide()
+            v.frame:show()
+        end
+    end)
+    y = y + 4
+end
 
--- ----------------------------------------
--- --MAIN FRAME
--- ----------------------------------------
+----------------------------------------
+--MAIN FRAME
+----------------------------------------
 
--- basalt.autoUpdate()
+basalt.autoUpdate()
 
--- ----------------------------------------
--- --TESTES
--- ----------------------------------------
+----------------------------------------
+--TESTES
+----------------------------------------
 
--- -- shell.run("/CC/src/GeneratorManager/GUI.lua")
--- -- local generatorModuleObject = require("/CC/src/GeneratorManager/Generator")
--- -- print(generatorModuleObject.getBatteryFillLevel())
+-- shell.run("/CC/src/GeneratorManager/GUI.lua")
+-- local generatorModuleObject = require("/CC/src/GeneratorManager/Generator")
+-- print(generatorModuleObject.getBatteryFillLevel())
