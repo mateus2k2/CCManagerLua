71716b38eca952a7b6e2714eaaa429eb2eccf36a
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 21:13:28 -0300

    update

--- src/GeneratorManager/GUI.lua
+++ src/GeneratorManager/GUI.lua
@@ -4,10 +4,13 @@ local generatorModule = require("/CC/src/GeneratorManager/Generator")
 
 function generatorGUIModule.createGeneratorFrame(main)
     local w, h = main:getSize()
+    local pageTitle = "Generator Manager"
+    local pageTitleLength = string.len(pageTitle)
+    local pageTitleX = math.floor(w / 2 - pageTitleLength / 2)
 
     local frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()
 
-    frame:addLabel():setText("Generator Manager"):setPosition(2, 2)
+    frame:addLabel():setText("Generator Manager"):setPosition(pageTitleX, 2)
 
     return main, frame
 end
