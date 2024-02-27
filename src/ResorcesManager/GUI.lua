2c8c197c1dd6207f80258bb865e1ff9d1b8290c0
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-26 21:06:00 -0300

    update

--- src/ResorcesManager/GUI.lua
+++ src/ResorcesManager/GUI.lua
@@ -4,7 +4,7 @@ local resorsesModuleMine = require("/CC/src/ResorcesManager/Resorces")
 function resorcesManagerGUIModule.createResorcesManagerFrame(main)
     frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}"):hide()
     frame:addLabel():setText("Resorce Menager"):setPosition(2, 2)
-    frame:addLabel():setText("Iron: " .. resorsesModuleMine.getIron()):setPosition(2, 2)
+    frame:addLabel():setText("Iron: " .. resorsesModuleMine.getIron()):setPosition(2, 3)
     return main, frame
 end
 
