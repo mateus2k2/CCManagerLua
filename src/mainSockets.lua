f4fda682132eeed37a719dce4d599bfe83faff90
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-02-28 18:12:07 -0300

    update

--- src/mainSockets.lua
+++ src/mainSockets.lua
@@ -1,47 +1,19 @@
-local socketPort = "5000"
-local serverURL = "ws://localhost:" .. socketPort .. "/"
--- local serverURL = "wss://ccapi.567437965.xyz/"
+local serverURL = "http://localhost:5000"
 
-local function handleWebSocketMessage(message)
-    local response = ""
+local function handleRequest(request)
 
-    print("Received message from client: " .. message)
-
-    if message == "energy" then
-        response = math.random(0, 100)
-    else 
-        response = "Invalid request"
-    end
-
-    return response
 end
 
 local function openWebSocket()
-    local ws, err = http.websocket(serverURL)
-
-    if not ws then
-        print("Failed to open websocket: " .. err)
-        return
-    end
-
-    print("Websocket opened successfully")
+    
+    print("Starting")
     print(serverURL)
 
-    local token = "123456789"
-    local isFirst = true
     while true do
-        if isFirst == true then
-            print("Sending token to server")
-            print(token)
-            ws.send(token)
-            isFirst = false
-        end
+        requests = http.get(serverURL .. "/getRequests")
+        print(requests)
 
-        local event, url, message = os.pullEvent("websocket_message")
-        response = handleWebSocketMessage(message)
-        ws.send(response)
 
-        --sleep for 1 second
         os.sleep(1)
         
     end
