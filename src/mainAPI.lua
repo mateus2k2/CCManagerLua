c8d1d34307606a93b2193c06729bebc829b764db
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-03-09 22:41:36 -0300

    update

--- src/mainAPI.lua
+++ src/mainAPI.lua
@@ -1,3 +1,5 @@
+-- Imports das APIs individuais
+
 local serverURL = "http://localhost:5000"
 
 local function handleRequest(request)
@@ -6,18 +8,14 @@ end
 
 local function openWebSocket()
     
-    print("Starting")
-    print(serverURL)
+    print("Starting" .. serverURL)
 
     while true do
         requests = http.get(serverURL .. "/getRequests")
         print(requests)
-
-
         os.sleep(1)
-        
     end
-
-end
+    
+end 
 
 openWebSocket()
