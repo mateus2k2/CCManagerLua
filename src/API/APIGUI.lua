0f437eb6ad04597ca1f7cefe54877390d6b66d18
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-03-11 15:31:10 -0300

    update

---
+++ src/API/APIGUI.lua
@@ -0,0 +1,11 @@
+local mainGUIModule = {}
+
+function mainGUIModule.createMainFrame(main)
+    frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")
+
+    frame:addLabel():setText("Main Page"):setPosition(2, 2)
+
+    return main, frame
+end
+
+return mainGUIModule
