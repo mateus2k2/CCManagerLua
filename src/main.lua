94b2c6ee72ef795962f940255bcff721de073ceb
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-10 14:18:17 -0300

    update

--- src/main.lua
+++ src/main.lua
@@ -79,7 +79,7 @@ function updateFrame()
         if sub[indexOnTop].updateFunction then
             sub[indexOnTop].updateFunction(sub[indexOnTop].objects)
         end
-        -- os.sleep(1)
+        os.sleep(1)
     end
 end
 
