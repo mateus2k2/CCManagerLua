ff26124a0e4334b31a03bf71661df64f242e24b0
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-10 20:00:05 -0300

    update

--- src/API/GUI.lua
+++ src/API/GUI.lua
@@ -3,6 +3,7 @@ local APIGUIModule = {}
 local APIModule = require("/CC/src/API/API")
 
 local logLine = 1
+local logCount = 1
 MyColors = {SUCCESS = colors.green, ERROR = colors.red, INFO = colors.yellow}
 
 function APIGUIModule.createAPIFrame(main)
@@ -43,12 +44,13 @@ end
 
 function APIGUIModule.updateFrame(objects)
     while true do
-        for i = logLine, #APIModule.logs do
+        for i = logCount, #APIModule.logs do
             for level, LogString in pairs(APIModule.logs[i]) do
                 for index, chunk in ipairs(breakString(LogString)) do
-                    objects.logsFrame:addLabel():setPosition(1, i+index-1):setText("#---------------------------------------------#"):setForeground(MyColors[level])
+                    objects.logsFrame:addLabel():setPosition(1, logLine):setText("#---------------------------------------------#"):setForeground(MyColors[level])
                     logLine = logLine + 1
                 end
+                logCount = logCount + 1
             end
         end
         os.sleep(1)
