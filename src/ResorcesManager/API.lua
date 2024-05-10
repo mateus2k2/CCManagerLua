834b95e632ab214a42e5c330cca38a11d862820b
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-10 11:16:55 -0300

    update

--- src/ResorcesManager/API.lua
+++ src/ResorcesManager/API.lua
@@ -3,7 +3,8 @@ local resorsesModuleAPI = {}
 local resorsesModuleMine = require("/CC/src/ResorcesManager/Resorces")
 
 function resorsesModuleAPI.handleRequest(request)
-    return {"teste": resorsesModuleMine.getIron()}
+    -- return {"teste": resorsesModuleMine.getIron()}
+    return {"teste": 123}
 end
 
 return resorsesModuleAPI
