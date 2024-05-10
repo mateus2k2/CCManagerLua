d8c619d8122d04061d35fb9aff0c0802caceb2ec
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-10 14:12:29 -0300

    update

--- src/API/GUI.lua
+++ src/API/GUI.lua
@@ -31,7 +31,7 @@ function APIGUIModule.updateFrame(objects)
     while true do
         for key, value in pairs(APIModule.logs) do
             for level, LogString in pairs(value) do
-                objects.logsFrame:addLabel():setPosition(3, key):setText(level):setForeground(MyColors[level])
+                objects.logsFrame:addLabel():setPosition(3, key):setText(LogString .. "111111111111111111111111111111111111111111111111111111111111111111111111111111"):setForeground(MyColors[level])
             end
         end
 
