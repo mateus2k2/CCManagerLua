13498369f1871eb3bcdb87cfc0e008ccf90888c2
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 11:39:01 -0300

    update

--- src/API/GUI.lua
+++ src/API/GUI.lua
@@ -35,7 +35,7 @@ function APIGUIModule.updateFrame(objects)
 
     for i = logCount, #APIModule.logs do
         for level, LogString in pairs(APIModule.logs[i]) do
-            for _, chunk in ipairs(uteisModule.breakString(LogString, size-2)) do
+            for _, chunk in ipairs(uteisModule.breakString(LogString, objects.size-2)) do
                 objects.logsFrame:addLabel():setPosition(2, logLine):setText(chunk):setForeground(MyColors[level])
                 logLine = logLine + 1
             end
