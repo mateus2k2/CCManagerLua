6310c1cee75d4d66e42eb93a03447fb04d10eee8
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 09:20:01 -0300

    update

--- src/GeneratorManager/GUI.lua
+++ src/GeneratorManager/GUI.lua
@@ -1,36 +1,34 @@
 local generatorGUIModule = {}
 
-local generatorModule = require("/CC/src/GeneratorManager/Generator")
+local Generator = require("/CC/src/GeneratorManager/Generator")
 
-function handleGeneratorButtom()
-    if generatorModule.getLock() then
-        if generatorModule.getStateGenerator() then 
-            generatorModule.turnOffGenerator()
+local function handleGeneratorButton()
+    if Generator.getLock() then
+        if Generator.getStateGenerator() then 
+            Generator.turnOffGenerator()
         else 
-            generatorModule.turnOnGenerator()
+            Generator.turnOnGenerator()
         end
-        generatorModule.releaseLock()
+        Generator.releaseLock()
     end
 end
 
 function generatorGUIModule.createGeneratorFrame(main)
     local w, h = main:getSize()
     local pageTitle = "Generator Manager"
-    local pageTitleLength = string.len(pageTitle)
-    local pageTitleX = math.floor(w / 2 - pageTitleLength / 2)
+    local pageTitleX = math.floor(w / 2 - #pageTitle / 2)
 
     local frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")
     
-    titleLabelVar = frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
-    batteryLabelVar = frame:addLabel():setText("Battery Energy: " .. generatorModule.getBatteryFillLevel()):setPosition(2, 5)
+    local titleLabel = frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
+    local batteryLabel = frame:addLabel():setText("Battery Energy: " .. Generator.getBatteryFillLevel()):setPosition(2, 5)
 
-    local generatorButtom = frame:addButton():setText("Generator is"):setPosition(pageTitleX, 8):onClick(handleGeneratorButtom):setBackground(colors.red):setBorder(colors.white):setSize(19, 3)
+    local generatorButton = frame:addButton():setText("Generator is"):setPosition(pageTitleX, 8):onClick(handleGeneratorButton):setBackground(colors.red):setBorder(colors.white):setSize(19, 3)
 
-
-    objects = {
-        title = titleLabelVar,
-        batteryLavel = batteryLabelVar,
-        generatorButtom = generatorButtom
+    local objects = {
+        title = titleLabel,
+        batteryLabel = batteryLabel,
+        generatorButton = generatorButton
     }
 
     return main, frame, objects
@@ -38,15 +36,15 @@ end
 
 function generatorGUIModule.updateFrame(objects)
     while true do
-        objects.batteryLavel:setText("Battery Energy: " .. generatorModule.getBatteryFillLevel())
+        objects.batteryLabel:setText("Battery Energy: " .. Generator.getBatteryFillLevel())
 
-        if generatorModule.getLock() then
-            if generatorModule.getStateGenerator() then 
-                objects.generatorButtom:setText("Generator is ON"):setBackground(colors.green)
+        if Generator.getLock() then
+            if Generator.getStateGenerator() then 
+                objects.generatorButton:setText("Generator is ON"):setBackground(colors.green)
             else
-                objects.generatorButtom:setText("Generator is OFF"):setBackground(colors.red)
+                objects.generatorButton:setText("Generator is OFF"):setBackground(colors.red)
             end
-            generatorModule.releaseLock()
+            Generator.releaseLock()
         end
         os.sleep(1)
     end
