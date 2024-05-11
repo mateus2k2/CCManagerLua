39989514abbb08f7b64a215ef26c3800d2f9ea3b
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 10:15:35 -0300

    update

--- src/Uteis/Debug.lua
+++ src/Uteis/Debug.lua
@@ -10,9 +10,8 @@ function debugModule.debugFunc(obj)
     if obj.first then
         local debugMenu = require('/CC/Modules/basaltDebug'):setBasalt(obj.basalt)
         debugFrame = debugMenu:createDebugMenu(obj.main, {'debugMenuFrame', 'debugMenuTextbox', 'debugMenuCheckbox'})
-    else    
-        return debugFrame
     end
+    return debugFrame
 end
 
 return debugModule
