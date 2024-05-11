7efdd27ba7c82a24291239c253dfe74892d6fe6b
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 08:54:39 -0300

    update

--- src/GeneratorManager/GUI.lua
+++ src/GeneratorManager/GUI.lua
@@ -17,7 +17,7 @@ function generatorGUIModule.createGeneratorFrame(main)
     titleLabelVar = frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
     batteryLabelVar = frame:addLabel():setText("Battery Energy: " .. generatorModule.getBatteryFillLevel()):setPosition(2, 5)
 
-    local generatorButtom = main:addButton():setText("Generator is"):setPosition(pageTitleX, 4):onClick(handleGeneratorButtom):setBackground(colors.red)
+    local generatorButtom = frame:addButton():setText("Generator is"):setPosition(pageTitleX, 4):onClick(handleGeneratorButtom):setBackground(colors.red)
 
 
     objects = {
