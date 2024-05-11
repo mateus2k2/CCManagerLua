20efc79a22914ff652c8f51e845d55bc9e6be348
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 08:49:14 -0300

    update

--- src/ResorcesManager/GUI.lua
+++ src/ResorcesManager/GUI.lua
@@ -2,8 +2,14 @@ local resorcesManagerGUIModule = {}
 local resorsesModuleMine = require("/CC/src/ResorcesManager/Resorces")
 
 function resorcesManagerGUIModule.createResorcesManagerFrame(main)
+    local pageTitle = "Main Page"
+    local pageTitleLength = string.len(pageTitle)
+    local pageTitleX = math.floor(w / 2 - pageTitleLength / 2)
+
     frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")
-    frame:addLabel():setText("Resorce Menager"):setPosition(2, 2)
+    
+    frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
+
     -- frame:addLabel():setText("Iron: " .. resorsesModuleMine.getIron()):setPosition(2, 3)
     return main, frame
 end
--- src/mainGUI.lua
+++ src/mainGUI.lua
@@ -1,9 +1,13 @@
 local mainGUIModule = {}
 
 function mainGUIModule.createMainFrame(main)
+    local pageTitle = "Main Page"
+    local pageTitleLength = string.len(pageTitle)
+    local pageTitleX = math.floor(w / 2 - pageTitleLength / 2)
+
     frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")
 
-    frame:addLabel():setText("Main Page"):setPosition(2, 2)
+    frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
 
     return main, frame
 end
