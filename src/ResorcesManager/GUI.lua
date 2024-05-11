591e07a296650b36b616405962b6076c04320f7c
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-11 09:40:36 -0300

    update

--- src/ResorcesManager/GUI.lua
+++ src/ResorcesManager/GUI.lua
@@ -16,8 +16,8 @@ function resorcesManagerGUIModule.createResorcesManagerFrame(main)
     
     frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
 
-    local searchField = frame:addInput():setPosition(2, 4):setSize(20, 2)
-    local searchButton = frame:addButton():setText("Search"):setPosition(2, 4):onClick(handleSearchButton):setBackground(colors.red):setSize(12, 2)
+    local searchField = frame:addInput():setPosition(2, 6):setSize(20, 2)
+    local searchButton = frame:addButton():setText("Search"):setPosition(2, 10):onClick(handleSearchButton):setBackground(colors.red):setSize(12, 2)
 
     local objects = {
         title = titleLabel,
