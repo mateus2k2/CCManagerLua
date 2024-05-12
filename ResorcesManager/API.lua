c9daee6a8528c17c5117ba6cf8aac2f4d97ec32b
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-12 20:44:27 -0300

    update

--- ResorcesManager/API.lua
+++ ResorcesManager/API.lua
@@ -6,7 +6,7 @@ function resorsesModuleAPI.handleRequest(request)
     result = nil
 
     if(request.name == nil) then 
-        return {result = "ERROR", errorType = "Error in the resorsesModuleAPI.handleRequest. Missing request.name"} end
+        return {result = "ERROR", errorType = "Error in the resorsesModuleAPI.handleRequest. Missing request.name"}
     else
         return {result = resorsesModuleMine.getItem(request.name)}
     end
