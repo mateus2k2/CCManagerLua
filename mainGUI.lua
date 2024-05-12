937b332064960c6b78460cf70bb16cac1a5fe631
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-12 19:38:41 -0300

    update

--- mainGUI.lua
+++ mainGUI.lua
@@ -6,7 +6,7 @@ function mainGUIModule.createMainFrame(main)
     local pageTitleLength = string.len(pageTitle)
     local pageTitleX = math.floor(w / 2 - pageTitleLength / 2)
 
-    frame = main:addFrame():setPosition(1, 1):setSize("{parent.w}", "{parent.h}")
+    frame = main:addFrame():setPosition(1, 1):setSize("parent.w", "parent.h")
 
     frame:addLabel():setText(pageTitle):setPosition(pageTitleX, 2)
 
