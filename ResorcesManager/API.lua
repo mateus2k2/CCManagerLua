0ae86960d918e8fd97ebc5490fc530dc02e3403b
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-12 20:15:34 -0300

    update

--- ResorcesManager/API.lua
+++ ResorcesManager/API.lua
@@ -3,7 +3,7 @@ local resorsesModuleAPI = {}
 local resorsesModuleMine = require("/CC/ResorcesManager/Resorces")
 
 function resorsesModuleAPI.handleRequest(request)
-    if(request.name == nil) then result = "Eroor in the resorsesModuleAPI.handleRequest"
+    if(request.name == nil) then result = "Eroor in the resorsesModuleAPI.handleRequest" end
 
     return resorsesModuleMine.getItem(request.name)
 end
