649701f8e2c2cd0179dbc0f2934f45cbda75dafe
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-12 22:22:02 -0300

    update

---
+++ mainGUI.lua
@@ -0,0 +1,86 @@
+local basalt = require("/Modules/basalt")
+
+local generatorGUIModule = require("/CC/GeneratorManager/GUI")
+local resorcesManagerGUIModule = require("/CC/ResorcesManager/GUI")
+local APIGUIModule = require("/CC/API/GUI")
+local mainPageModule = require("/CC/mainPage/GUI")
+
+indexOnTop = 1
+
+local function mainGUI() 
+    ----------------------------------------
+    --MODULES
+    ----------------------------------------
+
+
+    ----------------------------------------
+    --MAIN FRAME
+    ----------------------------------------
+
+    -- local monitors = peripheral.wrap("monitor_10") 
+    -- local main = basalt.addMonitor():setMonitor(monitors):setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
+
+    local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
+    local w, h = main:getSize()
+
+    local debugTestInMain = require("/CC/Uteis/Debug")
+    debugFrame = debugTestInMain.debugFunc({basalt = basalt, main = main, first = true})
+
+    ----------------------------------------
+    --SIDE BAR
+    ----------------------------------------
+
+    local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
+    :onGetFocus(function(self)
+        self:setPosition("{parent.w - (self.w-1) - 1}", 1)
+    end)
+    :onLoseFocus(function(self)
+        self:setPosition(w, 1)
+    end)
+
+    main, frameGenerator, generatorObjects = generatorGUIModule.createGeneratorFrame(main)
+    main, frameResorces, resoursesObjects = resorcesManagerGUIModule.createResorcesManagerFrame(main)
+    main, frameAPI, APIObjects = APIGUIModule.createAPIFrame(main)
+    main, frameMain = mainPageModule.createMainFrame(main)
+
+    local sub = {
+        {index = 1, frame = frameMain,             title = "Main",      objects = nil,              updateFunction = mainPageModule.updateFrame},
+        {index = 2, frame = frameAPI:hide(),       title = "API",       objects = APIObjects,       updateFunction = APIGUIModule.updateFrame},
+        {index = 3, frame = frameGenerator:hide(), title = "Generator", objects = generatorObjects, updateFunction = generatorGUIModule.updateFrame},
+        {index = 4, frame = frameResorces:hide(),  title = "Resorses",  objects = resoursesObjects, updateFunction = resorcesManagerGUIModule.updateFrame},
+    }
+
+
+    local y = 2
+    for k,v in pairs(sub) do
+        sidebar:addButton():setText(v.title)
+        :setBackground(colors.black)
+        :setForeground(colors.lightGray)
+        :setSize("{parent.w - 3}", 3)
+        :setPosition(2, y)
+        :onClick(function()
+            for a, b in pairs(sub)do
+                b.frame:hide()
+                v.frame:show()
+                indexOnTop = v.index
+            end
+        end)
+        y = y + 4
+    end
+end
+----------------------------------------
+--MAIN FRAME
+----------------------------------------
+
+function updateFrame()
+    while true do
+        sub[indexOnTop].updateFunction(sub[indexOnTop].objects)
+        os.sleep(1)
+    end
+end
+
+return {
+    mainGUI = mainGUI,
+    updateFrame = updateFrame,
+    basalt = basalt,
+}
--- mainSrc.lua
+++ mainSrc.lua
@@ -1,95 +1,19 @@
-indexOnTop = 1
-
-----------------------------------------
---MODULES
-----------------------------------------
-
-local basalt = require("/Modules/basalt")
-
-local generatorGUIModule = require("/CC/GeneratorManager/GUI")
-local resorcesManagerGUIModule = require("/CC/ResorcesManager/GUI")
-local APIGUIModule = require("/CC/API/GUI")
-local mainPageModule = require("/CC/mainPage/GUI")
-
-local API = require("/CC/API/API")
-
-----------------------------------------
---MAIN FRAME
-----------------------------------------
-
--- local monitors = peripheral.wrap("monitor_10") 
--- local main = basalt.addMonitor():setMonitor(monitors):setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
-
-local main = basalt.createFrame():setTheme({FrameBG = colors.lightGray, FrameFG = colors.black})
-local w, h = main:getSize()
-
-local debugTestInMain = require("/CC/Uteis/Debug")
-debugFrame = debugTestInMain.debugFunc({basalt = basalt, main = main, first = true})
-
-----------------------------------------
---SIDE BAR
-----------------------------------------
-
-local sidebar = main:addScrollableFrame():setBackground(colors.gray):setPosition(w, 1):setSize(15, "{parent.h}"):setZ(25):setDirection("vertical")
-:onGetFocus(function(self)
-    self:setPosition("{parent.w - (self.w-1) - 1}", 1)
-end)
-:onLoseFocus(function(self)
-    self:setPosition(w, 1)
-end)
-
-main, frameGenerator, generatorObjects = generatorGUIModule.createGeneratorFrame(main)
-main, frameResorces, resoursesObjects = resorcesManagerGUIModule.createResorcesManagerFrame(main)
-main, frameAPI, APIObjects = APIGUIModule.createAPIFrame(main)
-main, frameMain = mainPageModule.createMainFrame(main)
-
-local sub = {
-    {index = 1, frame = frameMain,             title = "Main",      objects = nil,              updateFunction = mainPageModule.updateFrame},
-    {index = 2, frame = frameAPI:hide(),       title = "API",       objects = APIObjects,       updateFunction = APIGUIModule.updateFrame},
-    {index = 3, frame = frameGenerator:hide(), title = "Generator", objects = generatorObjects, updateFunction = generatorGUIModule.updateFrame},
-    {index = 4, frame = frameResorces:hide(),  title = "Resorses",  objects = resoursesObjects, updateFunction = resorcesManagerGUIModule.updateFrame},
-}
-
-
-local y = 2
-for k,v in pairs(sub) do
-    sidebar:addButton():setText(v.title)
-    :setBackground(colors.black)
-    :setForeground(colors.lightGray)
-    :setSize("{parent.w - 3}", 3)
-    :setPosition(2, y)
-    :onClick(function()
-        for a, b in pairs(sub)do
-            b.frame:hide()
-            v.frame:show()
-            indexOnTop = v.index
-        end
-    end)
-    y = y + 4
-end
-
-----------------------------------------
---MAIN FRAME
-----------------------------------------
-
-function updateFrame()
-    while true do
-        sub[indexOnTop].updateFunction(sub[indexOnTop].objects)
-        os.sleep(1)
-    end
-end
+local APIModule = require("/CC/API/API")
+local mainGUIModule = require("/CC/mainGUI")
 
 local resorsesModuleAPI  = require("/CC/ResorcesManager/API")
 local generatorModuleAPI = require("/CC/GeneratorManager/API")
-modules = {
+modulesAPI = {
     resource = resorsesModuleAPI,
     generator = generatorModuleAPI
 }
 
+mainGUIModule.mainGUI()
+
 parallel.waitForAny(
-    function() updateFrame() end,
-    function() API.startAPI(modules) end,
-    function() basalt.autoUpdate() end
+    function() mainGUIModule.updateFrame() end,
+    function() mainGUIModule.basalt.autoUpdate() end,
+    function() APIModule.startAPI(modulesAPI) end
 )
 
 -- ----------------------------------------
