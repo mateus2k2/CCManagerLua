b61146f469cca8a32d58dffd32172bd3aceb059f
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-13 11:49:11 -0300

    update

--- Pages/APIPage/API.lua
+++ Pages/APIPage/API.lua
@@ -3,13 +3,21 @@ local APIHelperModule = {}
 local logFilePath = "/CC/Logs/logs.txt"
 local statusFilePath = "/CC/Logs/APIStatus.txt"
 
-function APIHelperModule.getLogs()
+function APIHelperModule.getLogs(startingLine)
     local logData = {}
     local file = fs.open(logFilePath, "r")
     if not file then
         return logData
     end
 
+    for _ = 1, startingLine - 1 do
+        if not file.readLine() then
+            file.close()
+            return logData
+        end
+    end
+
+    -- Read lines starting from the desired line
     local line = file.readLine()
     while line do
         local success, logEntry = pcall(textutils.unserialiseJSON, line)
