04b51dae774e5f961faa4d542a427106718200eb
mateus2k2 <mateusfilipe2k2@gmail.com>
2024-05-14 21:08:02 -0300

    update

--- Pages/ResorcesPage/Resorces.lua
+++ Pages/ResorcesPage/Resorces.lua
@@ -6,7 +6,7 @@ if rfSystem == nil then error("RF System not found") end
 function resorsesModuleMine.getItem(itemFilter)
     local returnVar = rfSystem.getItem({name = itemFilter})
 
-    if returnVar == nil then return {"Error"} end
+    if returnVar == nil then return {"Item Not Found"} end
     if returnVar == "nil" then return {"Item Not Found"} end
 
     return returnVar
@@ -15,7 +15,7 @@ end
 function resorsesModuleMine.getItemAmount(txtName)
     local returnVar = rfSystem.getItem({name = txtName})
 
-    if returnVar == nil then return "Error" end
+    if returnVar == nil then return "Item Not Found" end
     if returnVar == "nil" then return "Item Not Found" end
 
     return returnVar.amount
